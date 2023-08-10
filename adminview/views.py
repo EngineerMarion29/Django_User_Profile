10e5f1d503a9ff154cd6e657dd28df13ab292968
Marion De Guzman <mariondeguzman@GLMACM1470173.local>
2023-08-10 21:24:30 +0800

    updates aug10, 2023

--- adminview/views.py
+++ adminview/views.py
@@ -29,19 +29,36 @@ from .forms import UserProfileForm  # Import your new form
 
 def user_profile_list(request):
     query = request.GET.get('q')
+    education_query = request.GET.get('education')
+    recent_job_query = request.GET.get('recent_job')
+    years_of_experience_query = request.GET.get('years_of_experience')
+    previous_company_query = request.GET.get('previous_company')
+
     profiles = UserProfile.objects.all()
+
     if query:
         profiles = profiles.filter(
             Q(user__first_name__icontains=query) |
             Q(user__last_name__icontains=query) |
             Q(user__email__icontains=query) |
             Q(age__icontains=query) |
-            Q(position__icontains=query) |
-            Q(years_of_experience__icontains=query) |
-            Q(previous_company__icontains=query) |
             Q(gender__icontains=query)
         )
+
+    if education_query:
+        profiles = profiles.filter(education_course_name__icontains=education_query)
+
+    if recent_job_query:
+        profiles = profiles.filter(most_recent_job__icontains=recent_job_query)
+
+    if years_of_experience_query:
+        profiles = profiles.filter(years_of_experience=years_of_experience_query)
+
+    if previous_company_query:
+        profiles = profiles.filter(previous_company__icontains=previous_company_query)
+
     return render(request, 'adminview/user_profile_list.html', {'profiles': profiles})
+
 
 
 def delete_user_profiles(request):
@@ -116,13 +133,25 @@ def export_user_profiles_to_csv(request):
     response['Content-Disposition'] = 'attachment; filename="user_profiles.csv"'
 
     writer = csv.writer(response)
-    writer.writerow(['First Name', 'Last Name', 'Email Address', 'Age', 'Position', 'Years of Experience',
-                     'Previous Company', 'Resume', 'Gender'])
+    writer.writerow(['First Name', 'Last Name', 'Email Address', 'Age', 'Gender', 'Education Course Name',
+                     'Certifications', 'Most Recent Job', 'LinkedIn URL', 'Years of Experience', 'Previous Company',
+                     'Resume'])
 
     user_profiles = UserProfile.objects.all()
     for profile in user_profiles:
-        writer.writerow([profile.first_name, profile.last_name, profile.email_address, profile.age,
-                         profile.position, profile.years_of_experience, profile.previous_company,
-                         profile.resume.name, profile.gender])
+        writer.writerow([
+            profile.user.first_name,
+            profile.user.last_name,
+            profile.user.email,
+            profile.age,
+            profile.get_gender_display(),
+            profile.education_course_name,
+            profile.certifications,
+            profile.most_recent_job,
+            profile.linkedin_url,
+            profile.years_of_experience,
+            profile.previous_company,
+            profile.resume.name,
+        ])
 
     return response
